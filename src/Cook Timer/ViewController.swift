b43cbac9124d1fdecfa3ea10f30380e6638fd9fb
Karolina Guzewska <k.guzewska@gmail.com>
2017-12-31 19:32:21 +0100

    Init project

---
+++ src/Cook Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cook Timer
+//
+//  Created by Karolina Beata Natalia Guzewska on 31.12.2017.
+//  Copyright © 2017 Karolina Beata Natalia Guzewska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
