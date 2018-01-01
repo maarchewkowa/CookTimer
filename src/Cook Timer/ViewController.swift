7874359acdbed6473c50711cfde0f39bad508c71
Karolina Guzewska <k.guzewska@gmail.com>
2018-01-01 20:11:10 +0100

    time picker

--- src/Cook Timer/ViewController.swift
+++ src/Cook Timer/ViewController.swift
@@ -8,14 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    var timePicker: UIPickerView!
+    var toolBar: UIToolbar!
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 60
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(row)
+    }
+    
     var timer = Timer()
     var counter = 0
     @IBOutlet weak var timeLabel: UILabel!
-    
     @IBAction func okButtonPushed(_ sender: Any) {
-        counter = 5
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+        timePicker.isHidden = false
+        toolBar.isHidden = false
+        
+        
+    }
+    @objc func timeSelected (_ sender: UIBarButtonItem!) -> Void {
+        counter = timePicker.selectedRow(inComponent: 0) * 60 + timePicker.selectedRow(inComponent: 1)
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
             if (self.counter <= 0) {
                 print("Done")
                 timer.invalidate()
@@ -24,9 +42,37 @@ class ViewController: UIViewController {
                 self.counter -= 1
             }
         })
+        timePicker.isHidden = true
+        toolBar.isHidden = true
     }
     
     override func viewDidLoad() {
+        timePicker = UIPickerView(frame: CGRect(x: 0, y: 200, width: view.frame.width, height: 300))
+        timePicker.backgroundColor = .white
+        
+        timePicker.showsSelectionIndicator = true
+        timePicker.delegate = self
+        timePicker.dataSource = self
+        timePicker.isHidden = true
+        
+        toolBar = UIToolbar(frame: CGRect(x: 0, y: timePicker.frame.origin.y - 50, width: view.frame.width, height: 50))
+        toolBar.isHidden = true
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.plain, target: self, action: #selector (ViewController.timeSelected(_:)))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.plain, target: self, action: "donePicker")
+        
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        self.view.addSubview(toolBar)
+        self.view.addSubview(timePicker)
+        
+//        textField1.inputView = timePicker
+//        textField1.inputAccessoryView = toolBar
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
